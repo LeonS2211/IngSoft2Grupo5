a24bdc2ba8604dfa379d3889f885914dc2dfdb82
LeonS2211 <lgse1021@gmail.com>
2024-10-12 03:57:05 -0500

    Falta mover y probar con model/modelView

---
+++ Frontend/ecopoint-app/Models/ubicacionModel.ts
@@ -0,0 +1,9 @@
+export const markers = [
+  {
+    latitude: -12.085393051018592,
+    longitude: -76.97119693337255,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  },
+];
+export default markers;
